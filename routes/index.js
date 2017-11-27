f91814c1c9751c7ad2d657dbffeba43b03270fe2
waldemarthiel <waldemarthiel@users.noreply.github.com>
2017-11-27 22:40:38 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@
 /*
  * GET home page.
  */
+var os = require('os');
+var hostname = os.hostname();
 
 exports.index = function(req, res){
   res.render('index', {
         title: 'Express',
-        hostname:hostname
+        hostname: hostname
     });
 };
