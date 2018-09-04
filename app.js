e1db59210640b47104d0c8f09b5424a40b8b5249
wthiel <wthiel@WTHIEL-PL.pl.oracle.com>
2018-09-04 10:57:54 +0200

    test

--- app.js
+++ app.js
@@ -33,5 +33,5 @@ app.get('/', routes.index);
 app.post('/creditscore', creditscore.score);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Express server listening on port: ' + app.get('port'));
 });
